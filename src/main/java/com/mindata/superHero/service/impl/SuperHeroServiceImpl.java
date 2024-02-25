c71a4504277ac8748652ef3c60bbef12b6bdeece
Ruben Santopietro <rdsantopietro@gmail.com>
2024-02-25 01:42:34 -0300

    SuperHeroServiceImpl refactorizado

--- src/main/java/com/mindata/superHero/service/impl/SuperHeroServiceImpl.java
+++ src/main/java/com/mindata/superHero/service/impl/SuperHeroServiceImpl.java
@@ -3,6 +3,8 @@ package com.mindata.superHero.service.impl;
 import com.mindata.superHero.domain.SuperHero;
 import com.mindata.superHero.repository.SuperHeroRepository;
 import com.mindata.superHero.service.SuperHeroService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -10,8 +12,11 @@ import org.springframework.util.Assert;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class SuperHeroServiceImpl implements SuperHeroService {
+
+    private static Logger log = LoggerFactory.getLogger(SuperHeroServiceImpl.class);
 
     @Autowired
     private SuperHeroRepository superHeroRepository;
@@ -19,12 +24,14 @@ public class SuperHeroServiceImpl implements SuperHeroService {
 
     @Override
     public SuperHero save(SuperHero superHero) {
+        log.debug("Request to save SuperHero: {}", superHero);
         Assert.notNull(superHero, "Param shouldn't be null");
         return superHeroRepository.save(superHero);
     }
 
     @Override
     public SuperHero update(SuperHero superHero) {
+        log.debug("Request to update SuperHero: {}", superHero);
         Assert.notNull(superHero, "Param shouldn't be null");
         return superHeroRepository.save(superHero);
     }
@@ -32,21 +39,25 @@ public class SuperHeroServiceImpl implements SuperHeroService {
 
     @Override
     public List<SuperHero> findAll() {
+        log.debug("Request to find all SuperHeros");
         return superHeroRepository.findAll();
     }
 
     @Override
     public List<SuperHero> findAllByName(String name) {
+        log.debug("Request to find all SuperHeros by name: ", name);
         return superHeroRepository.findByNameContainingIgnoreCase(name);
     }
 
     @Override
     public Optional<SuperHero> findOne(Long id) {
+        log.debug("Request to find SuperHero by id : {}", id);
         return superHeroRepository.findById(id);
     }
 
     @Override
     public void delete(Long id) {
+        log.debug("Request to delete SuperHero by id : {}", id);
          superHeroRepository.deleteById(id);
     }
 }
